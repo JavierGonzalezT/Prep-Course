87e3188ec1e2c34949019f9f5a034ba1fbe82238
JavierGonzalezT <javiergtognoni@gmail.com>
2022-03-30 20:24:44 -0300

    Prueba

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,7 +8,6 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-  // PRUEBA GITHUB BORRAR!!
 }
 
 function agregarMetodoPrototype(Constructor) {
